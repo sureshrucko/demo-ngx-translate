9df14f3d1f8c3813ec67928261ee2654320ad630
suresh panigrahi <suresh.panigrahi@billmart.com>
2024-05-14 11:40:50 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'demo-ngx-translate';
+export class AppComponent implements OnInit, OnDestroy {
+
+  subscription: Subscription | undefined;
+
+  idFromCode = 'demo.text-in-code';
+
+  translatedMessageFromCode = "";
+  continuouslyTranslatedMessage = "";
+
+  constructor(private translate: TranslateService) {
+      translate.setDefaultLang('en');
+      translate.use('en');
+  }
+
+  ngOnInit() {
+      // translate message once
+      this.translate.get('demo.greeting', {name: 'John'}).subscribe((res: string) => {
+          this.translatedMessageFromCode = res;
+      });
+
+      // continuously update the property
+      this.subscription = this.translate.stream('demo.greeting', {name: 'John'}).subscribe((res: string) => {
+          this.continuouslyTranslatedMessage = res;
+      });
+  }
+
+  ngOnDestroy() {
+      this.subscription?.unsubscribe()
+  }
+
+  useLanguage(language: string) {
+      this.translate.use(language);
+  }
 }
